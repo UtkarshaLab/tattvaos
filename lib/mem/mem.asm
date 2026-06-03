5be4ed0a8517391e8d9e1d890ef6dbab687b4144
nplrajk <nplrajk@gmail.com>
2026-06-03 19:09:28 +0545

    build(mem): include pgtable_cache.asm in mem.asm

--- lib/mem/mem.asm
+++ lib/mem/mem.asm
@@ -23,6 +23,7 @@
 %include "lib/mem/phys/phys.asm"
 %include "lib/mem/heap/free_list.asm"
 %include "lib/mem/virt/pgtable.asm"
+%include "lib/mem/virt/pgtable_cache.asm"
 %include "lib/mem/virt/pgtable_map.asm"
 %include "lib/mem/virt/pgtable_unmap.asm"
 %include "lib/mem/virt/pgtable_walk.asm"
