270d470cba85e1f4c66358938a0ae41ef9131b20
nplrajk <nplrajk@gmail.com>
2026-06-06 21:15:26 +0545

    vmm: Define PAGE_ZSWAPPED flag in page directory walkthrough flags
    - Defined PAGE_ZSWAPPED as bit 11 (1 << 11) in PTE for Zswap detection.

--- lib/mem/virt/pgtable.asm
+++ lib/mem/virt/pgtable.asm
@@ -23,6 +23,7 @@ PAGE_DIRTY      equ (1 << 6)
 PAGE_HUGE       equ (1 << 7)        ; Huge page (PS bit in PDPT/PD)
 PAGE_GLOBAL     equ (1 << 8)
 PAGE_SWAPPED    equ (1 << 10)       ; Page is swapped out to mock swap device
+PAGE_ZSWAPPED   equ (1 << 11)       ; Page is compressed in Zswap cache
 PAGE_NX         equ (1 << 63)       ; No-execute
 
 section .text
