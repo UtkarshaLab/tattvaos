0ebe7f0318c2d26bc85a1a932c31bcddbc69b411
nplrajk <nplrajk@gmail.com>
2026-06-06 19:23:07 +0545

    refactor(mem): include kswapd.asm in memory assembly layout

--- lib/mem/mem.asm
+++ lib/mem/mem.asm
@@ -35,6 +35,7 @@
 %include "lib/mem/virt/thp.asm"
 %include "lib/mem/virt/swap_device.asm"
 %include "lib/mem/virt/swap.asm"
+%include "lib/mem/virt/kswapd.asm"
 %include "lib/mem/virt/pf.asm"
 %include "lib/mem/virt/replacement.asm"
 
