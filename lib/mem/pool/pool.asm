e017b6b1148859f75897e51610236b87f48b02a7
nplrajk <nplrajk@gmail.com>
2026-06-07 01:49:37 +0545

    lib/mem/pool/pool.asm: Include new pool_grow.asm and pool_destroy.asm modules

--- lib/mem/pool/pool.asm
+++ lib/mem/pool/pool.asm
@@ -17,7 +17,9 @@
 section .text
 
 %include "lib/mem/pool/pool_create.asm"
+%include "lib/mem/pool/pool_grow.asm"
 %include "lib/mem/pool/pool_alloc.asm"
 %include "lib/mem/pool/pool_free.asm"
+%include "lib/mem/pool/pool_destroy.asm"
 
 %endif ; LIB_MEM_POOL_POOL_ASM
