b6264174297a7dfa7d44fea7c3f5cbd8be75d0b4
nplrajk <nplrajk@gmail.com>
2026-06-11 21:48:34 +0545

    lib/mem: include vtlb.asm virtual TLB invalidation in mem.asm

--- lib/mem/mem.asm
+++ lib/mem/mem.asm
@@ -61,6 +61,7 @@
 %include "lib/mem/virt/pf.asm"
 %include "lib/mem/virt/ept.asm"
 %include "lib/mem/virt/spt.asm"
+%include "lib/mem/virt/vtlb.asm"
 
 %include "lib/mem/virt/replacement.asm"
 %include "storage/ummapf/mmap.asm"
