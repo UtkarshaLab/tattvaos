41ce10a10b0e9398db67fae18d908dfc13c242b7
nplrajk <nplrajk@gmail.com>
2026-06-12 16:13:54 +0545

    lib/mem/buddy: define buddy_node_t and implement active context swapping
    Virtualize the buddy allocator to support multiple memory node segments:
    - Define buddy_node_t structure storing segment boundaries (start_addr, end_addr),
      metadata order array, and free_heads array (12 orders) for each node instance.
    - Declare global buddy_nodes array (max 8 nodes) and node tracking index counters.
    - Implement buddy_save_context and buddy_load_context to swap the state of active
      allocator globals (buddy_start_addr, etc.) with node records, utilizing a lazy
      write-back context switch optimization.

--- lib/mem/buddy/buddy.asm
+++ lib/mem/buddy/buddy.asm
@@ -14,7 +14,100 @@
 
 %include "lib/mem/mem.inc"
 
+; Buddy node structure definition (128 bytes, power-of-two aligned)
+struc buddy_node_t
+    .start_addr     resq 1          ; Base physical address
+    .end_addr       resq 1          ; End physical address
+    .metadata       resq 1          ; Page order metadata pointer
+    .free_heads     resq 12         ; 12 free list head pointers (96 bytes)
+    .flags          resq 1          ; Status flags (bit 0 = active)
+endstruc
+
 section .text
+
+global buddy_save_context
+buddy_save_context:
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+
+    ; Index is in RAX (0-7)
+    imul rax, buddy_node_t_size
+    lea rdi, [buddy_nodes + rax]
+
+    ; Save start_addr, end_addr, metadata
+    mov rcx, [buddy_start_addr]
+    mov [rdi + buddy_node_t.start_addr], rcx
+    mov rcx, [buddy_end_addr]
+    mov [rdi + buddy_node_t.end_addr], rcx
+    mov rcx, [buddy_metadata]
+    mov [rdi + buddy_node_t.metadata], rcx
+
+    ; Save free_heads (12 qwords = 96 bytes)
+    lea rsi, [buddy_free_heads]
+    lea rdi, [rdi + buddy_node_t.free_heads]
+    mov rcx, 12
+    cld
+    rep movsq
+
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    ret
+
+global buddy_load_context
+buddy_load_context:
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+
+    ; Index is in RAX (0-7)
+    cmp rax, [buddy_active_node_index]
+    je .done
+
+    ; Save current active context if active index != -1
+    mov rdx, [buddy_active_node_index]
+    cmp rdx, -1
+    je .load_new
+
+    push rax
+    mov rax, rdx
+    call buddy_save_context
+    pop rax
+
+.load_new:
+    mov [buddy_active_node_index], rax
+    imul rax, buddy_node_t_size
+    lea rsi, [buddy_nodes + rax]
+
+    ; Load start_addr, end_addr, metadata
+    mov rcx, [rsi + buddy_node_t.start_addr]
+    mov [buddy_start_addr], rcx
+    mov rcx, [rsi + buddy_node_t.end_addr]
+    mov [buddy_end_addr], rcx
+    mov rcx, [rsi + buddy_node_t.metadata]
+    mov [buddy_metadata], rcx
+
+    ; Load free_heads (12 qwords = 96 bytes)
+    lea rsi, [rsi + buddy_node_t.free_heads]
+    lea rdi, [buddy_free_heads]
+    mov rcx, 12
+    cld
+    rep movsq
+
+.done:
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    ret
 
 %include "lib/mem/buddy/buddy_init.asm"
 %include "lib/mem/buddy/buddy_alloc.asm"
@@ -25,15 +118,23 @@ section .data
 global buddy_start_addr
 global buddy_end_addr
 global buddy_metadata
+global buddy_node_count
+global buddy_active_node_index
 
 buddy_start_addr: dq 0
 buddy_end_addr:   dq 0
 buddy_metadata:   dq 0
+buddy_node_count: dq 0
+buddy_active_node_index: dq -1
 
 section .bss
 
 align 8
 global buddy_free_heads
 buddy_free_heads: resb 12 * 8       ; 12 list heads for orders 0 to 11
+
+align 16
+global buddy_nodes
+buddy_nodes:      resb buddy_node_t_size * 8  ; Support up to 8 buddy allocator nodes
 
 %endif ; LIB_MEM_BUDDY_BUDDY_ASM
